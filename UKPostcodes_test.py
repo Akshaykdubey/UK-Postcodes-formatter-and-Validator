27f7f8611f30bb619d692115c4cd9705c5af9498
Akshay Dubey <akshay.dubey@ucdconnect.ie>
2018-09-18 11:18:42 +0100

    Update UKPostcodes_test.py

--- UKPostcodes_test.py
+++ UKPostcodes_test.py
@@ -1,6 +1,20 @@
 ##################################################################
-# @Description: This library is for formatting and validating UK
-#               Postcodes.
+# @Description: This unittest is to test the library 'UKPostcodes'
+#               for formatting and validating UK Postcodes.
+# @Testcases:
+#            1) test_format_postcode1 : 'Unit test to test the formatting the Postcode'
+#            2) test_format_postcode2 : 'Unit test to test the formatting the Postcode.
+#                                        Checks special character and throws an exception.'
+#            3) test_format_postcode3 : 'Unit test to test formatting of the Postcode
+#                                        Checks for length of Postcode and throws an exception
+#                                        if the the length doesn't lie between 6 and 8'
+#            3) test_validate_component_postcode1 : 'Unit test to validate the different
+#                                        components of the Postcode. Positive case
+#                                        Double Digit District'
+#            4) test_validate_component_postcode2 : 'Unit test to validate the different
+#                                        components of the Postcode. Positive case
+#                                        Single Digit District'
+#            5) test_validate_postcode1 : ''
 #
 #
 #
@@ -16,7 +30,7 @@ from UKPostcodes import PostCodes
 class TestPrintNumbers(unittest.TestCase):
 
     def test_format_postcode1(self):
-        """ Unit test for formatting of the Postcode
+        """ Unit test to test formatting of the Postcode
             Positive case
         """
         postcode_inst = PostCodes(10)
@@ -32,14 +46,48 @@ class TestPrintNumbers(unittest.TestCase):
         try:
             postcode_inst.format_postcode('PO16 #7GZ')
         except TypeError:
-            print("Expected an exception.\n"\
-                  "Postcode contains special character.\n"\
+            print("Expected an exception.\n"
+                  "Postcode contains special character.\n"
                   "Test2: Successful")
 
+    def test_format_postcode3(self):
+        """ Unit test to test formatting of the Postcode
+            Checks for length of Postcode and throws an exception
+            if the the length doesn't lie between 6 and 8
+        """
+        postcode_inst = PostCodes(10)
+        try:
+            postcode_inst.format_postcode('PO167')
+        except TypeError:
+            print("Expected an exception.\n"
+                  "Length of the Postcode should be between 6 and 8.\n"
+                  "Test3: Successful")
+
     def test_validate_component_postcode1(self):
+        """ Unit test to validate the different
+            components of the Postcode
+            Positive case Double Digit District
+        """
         postcode_inst = PostCodes(10)
         num = postcode_inst.validate_postcode_components('PO167GZ')
-        print(num.postcode_area, num.postcode_sector, num.postcode_unit, num.postcode_district)
+        assert num.postcode_area == 'PO' and num.postcode_district == '16'\
+               and num.postcode_sector == '7' and num.postcode_unit == 'GZ'
+        print("Postcode Area: " + num.postcode_area + "\nPostcode district: " + num.postcode_district + \
+              "\nPostcode sector: " + num.postcode_sector + "\nPostcode Unit: " + num.postcode_unit + \
+              "\nTest4: Successful")
+
+    def test_validate_component_postcode2(self):
+        """ Unit test to validate the different
+            components of the Postcode
+            Negative case
+        """
+        postcode_inst = PostCodes(10)
+        num = postcode_inst.validate_postcode_components('PO167GZ')
+        assert num.postcode_area == 'PO' and num.postcode_district == '16'\
+               and num.postcode_sector == '7' and num.postcode_unit == 'GZ'
+        print("Postcode Area: " + num.postcode_area + "\nPostcode district: " + num.postcode_district + \
+              "\nPostcode sector: " + num.postcode_sector + "\nPostcode Unit: " + num.postcode_unit + \
+              "\nTest5: Successful")
 
     def test_validate_postcode1(self):
         """ Unit test for validating of the Postcode
@@ -49,7 +97,62 @@ class TestPrintNumbers(unittest.TestCase):
 
         num = postcode_inst.validate_postcode('PO167GZ')
         assert num == True
-        print('Test3: Successful')
+        print('Test6: Successful')
+
+    def test_validate_postcode2(self):
+        """ Unit test for validating of the Postcode
+            Negative case
+        """
+        postcode_inst = PostCodes(10)
+
+        # The letters QVX are not used in the first position
+        num = postcode_inst.validate_postcode('VC1A 1BB')
+        assert num == False
+        print('Test7: Successful')
+
+    def test_validate_postcode3(self):
+        """ Unit test for validating of the Postcode
+            Negative case
+        """
+        postcode_inst = PostCodes(10)
+
+        # Only letters to appear in the third position are ABCDEFGHJKPSTUW
+        num = postcode_inst.validate_postcode('A9X 0AX')
+        assert num == False
+        print('Test8: Successful')
+
+    def test_validate_postcode4(self):
+        """ Unit test for validating of the Postcode
+            Negative case
+        """
+        postcode_inst = PostCodes(10)
+
+        # Only letters to appear in the fourth position are ABEHMNPRVWXY
+        num = postcode_inst.validate_postcode('AA9C 0AX')
+        assert num == False
+        print('Test9: Successful')
+
+    def test_validate_postcode5(self):
+        """ Unit test for validating of the Postcode
+            Negative case
+        """
+        postcode_inst = PostCodes(10)
+
+        # The letters IJZ are not used in the second position
+        num = postcode_inst.validate_postcode('VI1A 1BB')
+        assert num == False
+        print('Test10: Successful')
+
+    def test_validate_postcode6(self):
+        """ Unit test for validating of the Postcode
+            Negative case
+        """
+        postcode_inst = PostCodes(10)
+
+        # The final two letters do not use the letters CIKMOV
+        num = postcode_inst.validate_postcode('PO167GV')
+        assert num == False
+        print('Test11: Successful')
 
 
 
