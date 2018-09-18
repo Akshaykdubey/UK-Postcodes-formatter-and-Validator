a06fb769d5aa6e9786bc16bc8e7c1afc6b4fb53b
Akshay Dubey <akshay.dubey@ucdconnect.ie>
2018-09-18 12:07:24 +0100

    Update PrintingNumbers.py

--- PrintingNumbers.py
+++ PrintingNumbers.py
@@ -31,12 +31,18 @@
 def print_number():
     num = 101
     for i in range(1, num):
+        # Printing ThreeFive if number is divisible by 3 and 5 both
         if i % 3 == 0 and i % 5 == 0:
             print("ThreeFive")
+            
+        # Printing Three if number is divisible by 3
         elif i % 3 == 0:
             print("Three")
+            
+        # Printing five if number is divisible by 5
         elif i % 5 == 0:
             print("Five")
+        # Simply print number if none of the above conditions are met
         else:
             print(i)
 
